82c70f248fd317bbc42f2d234e6638746281adc8
David Young <tgs.wifi@gmail.com>
2016-02-27 14:11:49 +1100

    More python generation code

--- FunctionsImpl.py
+++ FunctionsImpl.py
@@ -3,36 +3,58 @@ tree = ET.parse('vk.xml')
 root = tree.getroot()
 
 class VkFunction:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
+        self.success = None
+        self.failed = None
         self.params = []
 
     def addParams(self, param):
         self.params.append(param)
 
+    def display(self):
+        print(self.name, self.returnType, self.success, self.failed)    
+        for param in self.params:
+            print('   ', param.tokens)
+
 class VkFunctionParam:
-    def __init__(self):
-        pass    
+    def __init__(self, name):
+        self.text = ''
+        self.tokens = []        
+        self.type = ''
+        self.name = name
+
+class VkClassMethod:
+    def __init__(self, fn):
+        self.name = fn.name[2:]
+        self.params = fn.params[1:]
+        self.fn = fn
+
+class VkClassObject:
+    def __init__(self, key):
+        self.key = key
+        self.name = key[2:]
+        self.methods = []
+
+    def addFn(self, fn):
+        self.methods.append(VkClassMethod(fn))
 
 functions = []
 
 commands = root.findall('commands/command')
 for child in commands:
-    f = VkFunction()
-    f.name = child.find('proto/name').text
+    name = child.find('proto/name').text    
+    f = VkFunction(name)
     f.returnType = child.find('proto/type').text
     
     success = child.attrib.get('successcodes')
 
-    if success is None:
-        f.success = None
-    else:
+    if not success is None:
         f.success = success.split(',')
 
     failed = child.attrib.get('errorcodes')
 
-    if failed is None:
-        f.failed = None
-    else:
+    if not failed is None:
         f.failed = failed.split(',')
         
     for param in child.findall('param'):
@@ -48,8 +70,36 @@ for child in commands:
 
 print ("Functions", len(functions) )
 
-for i in range(0,10):
-    f = functions[i]    
-    print(f.name, f.returnType, f.success, f.failed)    
-    for param in f.params:
-        print('   ', param.tokens)
+#for i in range(0,10):
+    #functions[i].display() 
+
+enumFn = lambda a: a.name.startswith('vkEnum')
+notFn = lambda a: not enumFn(a)
+
+# remaining = list(filter(notFn, functions))
+# print(len(remaining))
+
+loops = list(filter(enumFn, functions))
+# print('ENUMERATE FUNCTIONS', len(loops))
+
+#for l in loops:
+#    l.display()
+
+# INSTANCE FUNCTIONS
+instFn = lambda a: len(a.params[0].tokens) == 2 and not a.params[0].tokens[0].endswith('*')
+instances = list(filter(instFn, functions))
+print('INSTANCE FUNCTIONS', len(instances))
+
+chunks = {}
+for i in instances:
+    moduleName = i.params[0].tokens[0]
+    if not moduleName in chunks:
+        chunks[moduleName] = VkClassObject(moduleName)    
+    chunks[moduleName].addFn(i)    
+
+for key in chunks.keys():
+    item = chunks[key]
+    print(item.key, item.name, len(item.methods))
+
+    
+
